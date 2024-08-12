1bcd713eb5255a7651e6df42703aed439fad7620
Blake Burch <blakeburch@gmail.com>
2024-08-12 16:23:18 -0500

    Update settings.

--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,50 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Blake Burch - Data Strategy + AI Leader",
+  DESCRIPTION: "Get in touch with Blake Burch for projects related to data strategy or AI.",
+  EMAIL: "hello@blakeburch.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "Get in touch with Blake Burch for projects related to data strategy or AI.",
+};
+
+export const ABOUT: Metadata = {
+  TITLE: "About",
+  DESCRIPTION: "Learn more about my journey and experience.",
+};
+
+export const SPEAKING: Metadata = {
+  TITLE: "Speaking",
+  DESCRIPTION: "See how I talk about data strategy and AI on podcasts and at events.",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION: "A collection of musings on topics I am passionate about.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "A collection of my projects with walkthroughs and demos.",
 };
 
 export const SOCIALS: Socials = [
   {
     NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    HREF: "https://twitter.com/BlakeBurch_",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/blakeburch",
   },
   {
     NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
+    HREF: "https://www.linkedin.com/in/blakeburch",
   },
 ];
