0cfb36651c53e41d292bf9273aa838bacb742a11
Blake Burch <blakeburch@gmail.com>
2024-12-14 23:41:07 -0600

    Fix yarn build issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,6 +16,9 @@ export function formatDate(date: Date) {
 export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "");
   const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
-  return `${readingTimeMinutes} min read`;
+  const readingTimeMinutes = Math.ceil(wordCount / 200 + 1);
+  return {
+    text: `${readingTimeMinutes} min read`,
+    minutes: readingTimeMinutes
+  };
 }
