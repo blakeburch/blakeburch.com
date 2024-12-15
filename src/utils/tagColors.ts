be721f8032b765367516ba381871d67791d313a8
Blake Burch <blakeburch@gmail.com>
2024-12-15 12:36:28 -0600

    Update blog tag colors

--- src/utils/tagColors.ts
+++ src/utils/tagColors.ts
@@ -4,5 +4,9 @@ export const tagColors: { [key: string]: string } = {
   'presentation': 'bg-orange-200 text-orange-800 dark:bg-orange-800 dark:text-orange-200',
   'video': 'bg-red-200 text-red-800 dark:bg-red-800 dark:text-red-200',
   'webinar': 'bg-green-200 text-green-800 dark:bg-green-800 dark:text-green-200',
+  'bytes': 'bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-100',
+  'data': "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-100",
+  'action': "bg-fuchsia-100 text-fuchsia-800 dark:bg-fuchsia-900 dark:text-fuchsia-100",
+  'musings': 'bg-emerald-100 text-emerald-800 dark:bg-emerald-900 dark:text-emerald-100'
   // Add more tag colors as needed
 };
